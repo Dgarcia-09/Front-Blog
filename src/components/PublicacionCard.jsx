9dd177f29cc35ce77337fc438dd91dbdbcbe4991
¨Dgarcia-09¨ <dgarcia-2023532@kinal.edu.gt>
2025-05-09 21:03:14 -0600

    feat: se agrego la funcionalidad de filtrado por curso

--- src/components/PublicacionCard.jsx
+++ src/components/PublicacionCard.jsx
@@ -7,10 +7,20 @@ export const PublicacionCard = () => {
   const { publicaciones, loading, error } = useListarPublicaciones();
   const navigate = useNavigate();
   const [searchTerm, setSearchTerm] = useState('');
+  const [selectedCurso, setSelectedCurso] = useState('');
 
   if (loading) return <div className='loading'>Cargando...</div>;
   if (error) return <div className='error'>Error: {error}</div>;
   if (!publicaciones || !Array.isArray(publicaciones)) return <div className='error'>No se pudo cargar la lista de publicaciones</div>;
+
+
+  const cursos = [...new Set(publicaciones.map(pub => pub.curso))];
+
+  const publicacionesFiltradas = publicaciones
+    .filter((publicacion) =>
+      publicacion.titulo.toLowerCase().includes(searchTerm.toLowerCase()) &&
+      (selectedCurso ? publicacion.curso === selectedCurso : true)
+    );
 
   return (
     <div className="publicaciones-container">
@@ -20,25 +30,32 @@ export const PublicacionCard = () => {
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
+      
+      <select
+        value={selectedCurso}
+        onChange={(e) => setSelectedCurso(e.target.value)}
+      >
+        <option value="">Todos los cursos</option>
+        {cursos.map((curso, index) => (
+          <option key={index} value={curso}>{curso}</option>
+        ))}
+      </select>
+
       <div className="publicaciones-lista">
-        {publicaciones
-          .filter((publicacion) =>
-            publicacion.titulo.toLowerCase().includes(searchTerm.toLowerCase())
-          )
-          .map((publicacion) => (
-            <div
-              key={publicacion._id}
-              className="publicacion-card"
-              onClick={() => navigate(`/publicaciones/${publicacion._id}`)}
-            >
-              <h2>{publicacion.titulo}</h2>
-              <p>{publicacion.descripcion}</p>
-              <p className="publicacion-fecha">
-                Publicado el {new Date(publicacion.createdAt).toLocaleDateString()}
-              </p>
-              <p>{publicacion.curso}</p>
-            </div>
-          ))}
+        {publicacionesFiltradas.map((publicacion) => (
+          <div
+            key={publicacion._id}
+            className="publicacion-card"
+            onClick={() => navigate(`/publicaciones/${publicacion._id}`)}
+          >
+            <h2>{publicacion.titulo}</h2>
+            <p>{publicacion.descripcion}</p>
+            <p className="publicacion-fecha">
+              {publicacion.fecha}
+            </p>
+            <p>{publicacion.curso}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
